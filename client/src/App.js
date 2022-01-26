2d8dfdb3b9fd67544ea173ead49ba77b0bd729b1
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-26 19:08:19 +0600

    Alert Component & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 import "./App.css";
 
@@ -21,6 +22,7 @@ const App = () => (
           <Route exact path="/" element={<Landing />} />
         </Routes>
         <section className="container">
+          <Alert />
           <Routes>
             <Route exact path="/register" element={<Register />} />
             <Route exact path="/login" element={<Login />} />
