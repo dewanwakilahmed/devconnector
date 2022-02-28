9a70e033d4d188f9e213e03ddce28be8cffcdf8b
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-28 23:47:16 +0600

    Starting On The Dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 import setAuthToken from "../utils/setAuthToken";
@@ -100,6 +101,9 @@ export const login = (email, password) => async (dispatch) => {
 
 // logout / Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <img
+        src="https://tenor.com/bkMeX.gif"
+        alt="spinner"
+        style={{ width: "200px", margin: "auto", display: "block" }}
+      />
+    </div>
+  );
+};
+
+export default Spinner;
