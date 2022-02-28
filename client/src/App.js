82ad1e3faa6166d00d98b082d459f2d0a4e0f4f7
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-01 00:15:47 +0600

    CreateProfile Component

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import "./App.css";
@@ -45,6 +46,15 @@ const App = () => {
                 element={
                   <PrivateRoute>
                     <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/create-profile"
+                element={
+                  <PrivateRoute>
+                    <CreateProfile />
                   </PrivateRoute>
                 }
               />
